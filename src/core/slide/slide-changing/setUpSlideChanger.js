116764539258c3727256d5bf42bb503e5da2bd94
PiotrZdziarski <pzdziarski01@gmail.com>
2019-05-02 12:05:01 +0200

    adjusting core to new injector

--- src/core/slide/slide-changing/setUpSlideChanger.js
+++ src/core/slide/slide-changing/setUpSlideChanger.js
@@ -1,9 +1,14 @@
 import { FADE_IN_ANIMATION_TIME } from "../../constants/cssConstants";
+import { TimeoutQueue } from "../timeouts/TimeoutQueue";
 
 export function setUpSlideChanger(
     {
         componentsStates: { slide: slideState },
+        injector: {
+            injectDependency
+        },
         core: {
+            stage,
             sourceAnimator,
             sourceHoldersTransformer,
             slideChanger: self
@@ -12,16 +17,19 @@ export function setUpSlideChanger(
 ) {
     let previousSlideNumber;
     let newSlideNumber;
-    let wasSlideChangedDuringAnimationArray = [];
+    const previousSourceNegativeTransformQueue = injectDependency(TimeoutQueue);
+    const removeFadeOutQueue = injectDependency(TimeoutQueue);
 
     self.changeSlideTo = (newSlide) => {
         previousSlideNumber = slideState.get();
         newSlideNumber = newSlide;
-        animateSourceHolders();
         slideState.set(newSlideNumber);
         slideState.onUpdate = () => {
             sourceHoldersTransformer.transformStageSourceHolders().withTimeout();
         };
+        animateSourceHolders();
+        removeFadeOutFromAllSourcesAfterTimeout();
+        ifPreviousSlideIsNotInStageTransformItNegativeAfterTimeout();
     };
 
     const animateSourceHolders = () => {
@@ -29,17 +37,26 @@ export function setUpSlideChanger(
         sourceAnimator.animateSourceFromSlide(previousSlideNumber).fadeOut();
         sourceAnimator.animateSourceFromSlide(newSlideNumber).removeFadeOut();
         sourceAnimator.animateSourceFromSlide(newSlideNumber).fadeIn();
-        removeFadeOutFromAllSourcesAfterTimeout();
     };
 
     const removeFadeOutFromAllSourcesAfterTimeout = () => {
         wasSlideChangedDuringAnimationArray.push(true);
         setTimeout(() => {
             wasSlideChangedDuringAnimationArray.pop();
-            if (wasSlideChangedDuringAnimationArray.length !== 0) {
-                return;
+            if (wasSlideChangedDuringAnimationArray.length === 0) {
+                sourceAnimator.removeFadeOutFromAllSources();
             }
-            sourceAnimator.removeFadeOutFromAllSources();
         }, FADE_IN_ANIMATION_TIME);
+    };
+
+    const ifPreviousSlideIsNotInStageTransformItNegativeAfterTimeout = () => {
+        const previousSlideIndex = previousSlideNumber - 1;
+        const timeoutQueue = injectDependency(TimeoutQueue);
+        timeoutQueue.time = FADE_IN_ANIMATION_TIME - 30;
+        timeoutQueue.condition = !stage.isSourceInStage(previousSlideIndex);
+        timeoutQueue.action = () => {
+            sourceHoldersTransformer.transformSourceHolderAtIndex(previousSlideIndex).negative();
+        };
+        timeoutQueue.startTimeout();
     };
 }
