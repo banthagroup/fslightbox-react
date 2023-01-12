154b5ca96eb34bdb05575db50b60f922f8e05587
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-12 20:09:00 +0100

    Removed unused code from the "demo/DemoComponent.jsx".
    The demo now contains a standard example with all 5 types of sources—an image, an HTML video, a YouTube video, a Vimeo video (a custom source), an invalid source.

--- demo/DemoComponent.jsx
+++ demo/DemoComponent.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import FsLightbox from '../FsLightbox.jsx';
+import React, { Component } from "react";
+import FsLightbox from "../FsLightbox.jsx";
 
 class DemoComponent extends Component {
     constructor(props) {
@@ -9,12 +9,10 @@ class DemoComponent extends Component {
             lightboxController: {
                 toggler: false,
                 slide: 1
-            },
-            key: 0
+            }
         };
 
         this.openLightboxOnSlide = this.openLightboxOnSlide.bind(this);
-        this.remountLightbox = this.remountLightbox.bind(this);
     }
 
     openLightboxOnSlide(number) {
@@ -26,41 +24,34 @@ class DemoComponent extends Component {
         });
     }
 
-    remountLightbox() {
-        this.setState({
-            key: this.state.key + 1
-        });
-    }
-
     render() {
         return (
             <>
                 <button onClick={() => this.openLightboxOnSlide(1)}>
-                    Open Lightbox on slide 1
+			1
                 </button>
                 <button onClick={() => this.openLightboxOnSlide(2)}>
-                    Open Lightbox on slide 2
-                </button>
-                <button onClick={this.remountLightbox}>
-                    Remount lightbox
+			2
                 </button>
 
                 <FsLightbox
                     toggler={this.state.lightboxController.toggler}
                     sources={[
-                        <audio src="/demo/videos/audio.m4a" controls />
-                    ]}
-                    customAttributes={[
-                        {
-                            srcSet: '/demo/img/7.jpg 600w, /demo/img/5.jpg 1200w',
-                            sizes: '(max-width: 600px) 600px, 1200px'
-                        }
+			"demo/img/1.jpeg",
+			"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+			"https://www.youtube.com/watch?v=3nQNiWdeH2Q",
+			<iframe
+				src="https://player.vimeo.com/video/22439234"
+				id="vimeo"
+				width="1920px"
+				height="1080px"
+				frameBorder="0"
+				allow="autoplay; fullscreen"
+				allowFullScreen
+			/>,
+			""
                     ]}
                     slide={this.state.lightboxController.slide}
-                    disableLocalStorage={true}
-                    maxYoutubeVideoDimensions={{ width: 500, height: 500 }}
-                    loadOnlyCurrentSource={true}
-                    key={this.state.key}
                 />
             </>
         );
