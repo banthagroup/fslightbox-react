67c0cef2c9715d19600ddfdc646834f0a36c31ec
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-12 12:13:45 +0100

    Removed Cypress and integration testing.
    And all files and packages associated with that. The "testVars.js" file from the "tests/__tests-services__" directory was saved. The Jest framework and packages and files associated with it weren't yet removed; they will be removed during the comming next removal of all unit tests.

--- tests/__tests-services__/resolvePropertyChainAtObject.js
+++
@@ -1,5 +0,0 @@
-export function resolvePropertyChainAtObject(propertyChain, object) {
-    return propertyChain.split('.').reduce(function (prev, curr) {
-        return prev ? prev[curr] : null
-    }, object)
-}
