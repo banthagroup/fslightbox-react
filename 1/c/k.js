cb9bfea3776e71526409575daf16245c449c3280
piotrzdziarski <pzdziarski@protonmail.com>
2023-04-13 19:54:19 +0200

    Fix an undefined variable error in the "k" function.

--- 1/c/k.js
+++ 1/c/k.js
@@ -1,1 +1,1 @@
-export function k({core:{lightboxCloser,slideChangeFacade},fs}){switch(e.key){case"Escape":lightboxCloser.closeLightbox();break;case"ArrowLeft":slideChangeFacade.changeToPrevious();break;case"ArrowRight":slideChangeFacade.changeToNext();break;case"F11":e.preventDefault();fs.t()}}
+export function k({core:{lightboxCloser,slideChangeFacade},fs},e){switch(e.key){case"Escape":lightboxCloser.closeLightbox();break;case"ArrowLeft":slideChangeFacade.changeToPrevious();break;case"ArrowRight":slideChangeFacade.changeToNext();break;case"F11":e.preventDefault();fs.t()}}
