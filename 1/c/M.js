74546ea5387ef01cd764aaed76d4aefde10f7d9c
piotrzdziarski <pzdziarski@protonmail.com>
2023-04-14 11:35:01 +0200

    Squeeze the "M" object.

--- 1/c/M.js
+++ 1/c/M.js
@@ -1,15 +1,1 @@
-import { ma } from "./ma";
-import { fr } from "../h/fr";
-
-export function M(o) {
-    var { p, sl } = o,frf = fr();
-
-    (sl === 1) ?
-        this.a = function () {
-            p.swipedX = 1
-        } :
-        this.a = function (e) {
-            if (p.isSwiping && frf())
-                ma(o, e)
-        }
-}
+import{ma}from"./ma";import{fr}from"../h/fr";export function M(o){var{p,sl}=o,frf=fr();(sl===1)?this.a=function(){p.swipedX=1}:this.a=function(e){if(p.isSwiping&&frf())ma(o,e)}}
