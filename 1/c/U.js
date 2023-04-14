2f2bca679bcb971f81f3d1033a32bd5d7dd51465
piotrzdziarski <pzdziarski@protonmail.com>
2023-04-14 11:30:32 +0200

    Squeeze the "U" object.

--- 1/c/U.js
+++ 1/c/U.js
@@ -1,13 +1,1 @@
-import { Ua } from "./Ua";
-
-export function U({ p,r }) {
-    var a = r(Ua);
-
-    this.a = function () {
-        if (p.isSwiping) {
-            (p.swipedX) ?
-                a.s() :
-                a.n();
-        }
-    }
-}
+import{Ua}from"./Ua";export function U({p,r}){var a=r(Ua);this.a=function(){if(p.isSwiping){(p.swipedX)?a.s():a.n()}}}
