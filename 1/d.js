a111c23712749b211f21dc540e3cda8c58f3484c
piotrzdziarski <pzdziarski@protonmail.com>
2025-08-22 19:43:10 +0200

    2.0.0

--- 1/d.js
+++ 1/d.js
@@ -1,17 +1,17 @@
 import T from"./T.jsx";import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import ReactDOMServer from "react-dom/server";
-import O from "./o.jsx";class B extends Component {render(){return <div style={{width:"1000px",height:"500px",background:"yellow"}}>B</div>}}
-
+import O from "../o.jsx";class B extends Component {render(){return <div style={{width:"500px",height:"1000px",background:"yellow"}}>B</div>}}
+var S=[["t/2.jpg"],["t/ocean.jpg"]];
 class DemoComponent extends Component {
-    constructor(props) {
+    	constructor(props) {
         super(props);
 
         this.state = {
             lightboxController: {
                 toggler: false,
                 slide: 1
-            }
+            },k:0,b:0
         };
 
         this.openLightboxOnSlide = this.openLightboxOnSlide.bind(this);
@@ -32,25 +32,14 @@ class DemoComponent extends Component {
                 <button onClick={() => this.openLightboxOnSlide(1)}>
 			1
                 </button>
-                <button onClick={() => this.openLightboxOnSlide(2)}>
-			2
-                </button>                <button onClick={() => this.openLightboxOnSlide(3)}>
-			3
+                <button onClick={()=>{/*setTimeout(()=>{this.setState({b:this.state.b+1})});setTimeout(()=>{this.setState({b:this.state.b+5})},50);*/var k=this.state.k;this.setState({k:k+1})}}>
+			U
                 </button><T/>
 
                 <O
                     toggler={this.state.lightboxController.toggler}
-                    sources={[
-			"t/1.jpg","t/1.jpg"
-                    ]}
-                    slide={this.state.lightboxController.slide} autoplay
-                />
-		<O
-                    toggler={this.state.lightboxController.toggler}
-                    sources={[
-			"t/1.jpg","t/1.jpg"
-                    ]}
-                    slide={this.state.lightboxController.slide} autoplay
+                    sources={S[this.state.k]}
+                    slide={this.state.lightboxController.slide} autoplay openOnMount={false}disableLocalStorage={true}exitFullscreenOnClose={false}loadOnlyCurrentSource={true}disableSlideSwiping={true}key={this.state.k}
                 />
             </>
         );
---
+++ o.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from "react";
+
+var r=0;
+if (typeof window==="object"){require("../../1/1/o.js");r=1}
+
+export default function (p) {
+	var c = useRef(), s = [], o = useRef(), g = useRef(p.toggler);
+	for (var j = 0; j < p.sources.length; j++)
+		s[j] = useRef();
+        function op(){var j=p.sourceIndex;if(j===undefined)j=p.slide;if(j)j--;else j=0;o.current.open(j)}
+	if (React.useInsertionEffect)
+		React.useInsertionEffect(()=>{if(!r){require("../../1/1/o.js");r=1}},[]);
+	useEffect((b) => {
+		if(!r){require("../../1/1/o.js");r=1}
+		if (!o.current) {
+			var t = new window.FsLightbox();
+			Object.assign(t.props, p);
+			t.props.sources=p.sources.map((e,j)=>{if(typeof e=="string")return e;c.current.removeChild(s[j].current);return s[j].current});t.props.onSourceLoad=(o,e,i)=>{if(typeof p.sources[i]!="string"){e.firstChild.style.width="100%";e.firstChild.style.height="100%"}if(p.onSourceLoad)p.onSourceLoad(o,e,i)};t.props.maxYoutubeDimensions=p.maxYoutubeVideoDimensions;o.current=t;
+                        if (p.openOnMount) op();
+                        return
+		}
+		if (p.toggler!=g.current){g.current=p.toggler;op()}
+	}, [p.toggler]);
+	return !o.current&&<div ref={c} style={{display:"none"}}>{p.sources.map((e,j)=>typeof e!="string"&&<div ref={s[j]} className="fslightboxs"key={j}>{e}</div>)}</div>
+}
+// The storage of old toggler's value in a ref is required due to double call of "useEffect" in, e.g., Next.js@15.4.1.
